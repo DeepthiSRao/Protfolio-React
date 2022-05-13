38ed1fa8ae5d88f2f94969697637d0a8c9f3aff0
Deepthi Srinidhi Rao <deepthi2609@gmail.com>
2022-05-12 18:04:00 -0700

    First commit

---
+++ src/util/project_data.js
@@ -0,0 +1,16 @@
+export const data = [
+    {
+        title : 'Pokedex',
+        desc: 'A Pokédex responsive web app developed using HTML, CSS and Javascript. The app fetches Pokémon data from external API and displays a list of Pokémon. The list is searchable by name. On click of pokemon, modal is displayed with Pokémon sprites, height and type.',
+        codeLink: 'https://github.com/DeepthiSRao/pokemon-js-app',
+        demo: 'https://deepthisrao.github.io/pokemon-js-app/',
+        image: ''
+    }, 
+    {
+        title : 'myFlix',
+        desc: 'MyFlix is a client side application built using ReactJS for its existing server-side code. MyFlix app is a responsive app that displays a list of movies for registered users. Users are allowed to view the movie details, director info, genre info, favorite a movies and search movie by title.',
+        codeLink: 'https://github.com/DeepthiSRao/pokemon-js-app',
+        demo: 'https://deepthisrao.github.io/pokemon-js-app/',
+        image: './image/project/myFlix_react.png' 
+    }
+];
